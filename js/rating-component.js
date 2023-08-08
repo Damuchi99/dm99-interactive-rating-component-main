a37b95571e695c17e4248535e08e745061c1146c
Damuchi99 <damuchi99@gmail.com>
2023-08-07 23:29:36 -0300

    Agrego comportamiento de javascript. Primera función: Al clickear SUBMIT, sin importar el puntaje, aparece la ventana de agradecimiento.

---
+++ js/rating-component.js
@@ -0,0 +1,5 @@
+document.getElementById('boton-enviar').addEventListener('click', function(){
+    //let puntaje (obtener elemento seleccionado)
+    document.getElementById('puntuacion').style.display = 'none';
+    document.getElementById('agradecimiento').style.display = 'grid';
+});
